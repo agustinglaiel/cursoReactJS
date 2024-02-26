caf69ab4c7056587eb48d8d1ade2bf902f7acc9d
Agustin Glaiel <agustinglaiel@gmail.com>
2024-02-26 16:25:30 -0300

    calse 3

--- src/models/levels.enum.js
+++ src/models/levels.enum.js
@@ -1,5 +1,8 @@
 export const LEVELS = {
-    NORMAL : 'normal',
-    URGENT : 'urgente',
-    BLOCKING : 'blocking'
-}
+  NORMAL: "normal",
+  URGENT: "urgente",
+  BLOCKING: "blocking",
+};
+
+//Estos son levels de tareas para task.clas.js
+//definen ciertas claves y se le asigna valores
