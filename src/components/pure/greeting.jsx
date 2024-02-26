837b2d8a0a0bfb86b64868d7d3e1a37be813b71d
Agustin Glaiel <agustinglaiel@gmail.com>
2024-02-26 16:05:05 -0300

    mensajes

--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -1,46 +1,47 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'; //nos sirven para especificar el tipo del dato que le pasamoos.
+import React, { Component } from "react";
+import PropTypes from "prop-types"; //nos sirven para especificar el tipo del dato que le pasamos.
 
+class Greeting extends Component {
+  //las clases las ponemos en mayusculas.
+  constructor(props) {
+    //props son propiedades o atributos que le damos a los componentes.
+    super(props); //informacion que pasamos por atributos.
+    this.state = {
+      //esto es algo privado del componente y no lo podemos cambiar a menos que usemos una funcion especifica.
+      age: 23,
+    };
+  }
 
-class Greeting extends Component { //las clases las ponemos en mayusculas. 
-    constructor(props){ //props son propiedades o atributos que le damos a los componentes. 
-        super(props); //informacion que pasamos por atributos.
-        this.state = { //esto es algo privado del componente y no lo podemos cambiar a menos que usemos una funcion especifica. 
-            age : 29 
-        }
-    }
+  render() {
+    return (
+      //en jsx no podemos renderizar mas de un elemento padre (Ej: no mas de un div).
+      <div>
+        <h1>
+          Hola {this.props.name}!{" "}
+          {/*aca concatenamos codigo de javascrips, por eso usamos llaves*/}
+        </h1>
+        <h2>Tu edad es {this.state.age} años.</h2>
+        <div>
+          <button onClick={this.birthday}>
+            {" "}
+            {/*le estamos pasando un evento*/}
+            Cumplir años
+          </button>
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        return ( //en jsx no podemos renderizar mas de un elemento padre (Ej: no mas de un div). 
-            <div> 
-                <h1>
-                    Hola {this.props.name}! 
-                </h1>
-                <h2>
-                    Tu edad es {this.state.age} años.
-                </h2>
-                <div>
-                    <button onClick={this.birthday}> {/*le estamos pasando un evento*/}
-                        Cumplir años 
-                    </button>
-                </div>
-            </div>
-        );
-    }
-
-    birthday = () => {
-        this.setState((prevState) => (
-                {
-                    age : prevState.age + 1
-                }
-            ))
-    }
+  birthday = () => {
+    this.setState((prevState) => ({
+      age: prevState.age + 1,
+    }));
+  };
 }
 
-
-Greeting.propTypes = { //contenido que le paso a un componente desde un componente superior. 
-    name: PropTypes.string, //le decimos que name es una prop de tipo string. 
+Greeting.propTypes = {
+  //contenido que le paso a un componente desde un componente superior.
+  name: PropTypes.string, //le decimos que name es una prop de tipo string.
 };
-
 
 export default Greeting;
--- src/components/pure/greetingF.jsx
+++ src/components/pure/greetingF.jsx
@@ -1,39 +1,33 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const GreetingF = (props) => {
-    //Breve intro a useState
-    //const [variable, metodo para actualizarlo] = useState(valor inicial).
-    const [age, setage] = useState(29);
+  //Breve intro a useState
+  //const [variable, metodo para actualizarlo] = useState(valor inicial).
+  const [age, setage] = useState(23);
 
-    const birthday = () => {
-        //actulizamos el State
-        setage(age + 1)
-    }
+  const birthday = () => {
+    //actulizamos el State
+    setage(age + 1);
+  };
 
-    return (
-        <div> 
-            <h1>
-                Hola {props.name} desde componente funcional! 
-            </h1>
-            <h2>
-                Tu edad es {age} años.
-            </h2>
-            <div>
-                <button onClick={birthday}> {/*le estamos pasando un evento*/}
-                    Cumplir años 
-                </button>
-            </div> 
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hola {props.name} desde componente funcional!</h1>
+      <h2>Tu edad es {age} años.</h2>
+      <div>
+        <button onClick={birthday}>
+          {" "}
+          {/*le estamos pasando un evento*/}
+          Cumplir años
+        </button>
+      </div>
+    </div>
+  );
 };
-
 
 GreetingF.propTypes = {
-    name : PropTypes.string
+  name: PropTypes.string,
 };
-
 
 export default GreetingF;
